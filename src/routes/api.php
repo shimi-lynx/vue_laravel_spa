e9784087b4fde7a106f7b87ba5fea46c6f3659a9
shimi-lynx <n33etc@gmail.com>
2020-05-13 16:36:00 +0900

    feat: ログアウトAPIの作成
    - Laravelの標準middlewareをカスタマイズして使用
    - LoginControllerの設定
    - ログアウトルーティングの設定
    - テストコードの作成

--- src/routes/api.php
+++ src/routes/api.php
@@ -5,3 +5,5 @@ use Illuminate\Http\Request;
 Route::post('/register', 'Auth\RegisterController@register')->name('register');
 // ログイン
 Route::post('/login', 'Auth\LoginController@login')->name('login');
+// ログアウト
+Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
