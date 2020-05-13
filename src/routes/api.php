2645912f042999b4bfdb01de9caff125ee0a9b29
shimi-lynx <n33etc@gmail.com>
2020-05-13 16:23:29 +0900

    feat: ログインAPIの作成
    - Laravelの標準middlewareを使用
    - ログインAPIのルート設定
    - LoginControllerの設定
    - テストコードの作成

--- src/routes/api.php
+++ src/routes/api.php
@@ -3,3 +3,5 @@
 use Illuminate\Http\Request;
 
 Route::post('/register', 'Auth\RegisterController@register')->name('register');
+// ログイン
+Route::post('/login', 'Auth\LoginController@login')->name('login');
