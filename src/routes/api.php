d53de904be959a9f696dcfe385ec84e641d5a7ad
shimi-lynx <n33etc@gmail.com>
2020-05-27 16:56:06 +0900

    feat: 写真投稿APIの作成

--- src/routes/api.php
+++ src/routes/api.php
@@ -9,3 +9,5 @@ Route::post('/login', 'Auth\LoginController@login')->name('login');
 Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
 // ログインユーザー
 Route::get('/user', fn () => Auth::user())->name('user');
+// 写真投稿
+Route::post('/photos', 'PhotoController@create')->name('photo.create');
